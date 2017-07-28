f9248f7b9ef8ebc9ea0edb85be73d465e22c5110
PC\Admin <bxcau97@gmail.com>
2017-07-28 20:28:55 +0700

    nop bai buoi 2

---
+++ buoi 2/52625.py
@@ -0,0 +1,13 @@
+import time
+import webbrowser
+while True :
+    for i in range (1,20):
+        time.sleep(1)
+
+       
+       
+        
+        print (" con ",20-i,time.ctime())
+        
+
+    webbrowser.open(" https://www.youtube.com/watch?v=04854XqcfCY", time.ctime())
